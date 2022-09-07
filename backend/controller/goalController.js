6deffc8ec3fbef70c8ed2d0315eedac01f813049
Angelo Maiele <angelomaiele@gmail.com>
2022-09-07 12:27:11 -0400

    routes working fine

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -21,12 +21,30 @@ const createGoals = asyncHandler(async(req, res) => {
 })
 
 const updateGoals = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `edit goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error ('goal not found')
+    }
+    const updatedGoal =  await Goal.findByIdAndUpdate(req.params.id,  req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 
 const deleteGoals = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `delete goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error ('goal not found')
+    }
+    await goal.remove()
+
+    res.status(200).json({id : req.params.id})
 })
 
 
