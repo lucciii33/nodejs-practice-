0e13f73ecfed8520fafc3b4815dc226c59503d8a
Angelo Maiele <angelomaiele@gmail.com>
2022-09-16 15:25:42 -0400

    more autentication

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import {FaUser} from 'react-icons/fa'
 
 export default function Register() {
   const [formData, setFormData] =  useState({
@@ -9,7 +10,45 @@ export default function Register() {
     password2: "",
   })
   const {name,email,password,password2} = formData;
+  const onChange = ()=>{
+
+  }
   return (
-    <div>Register</div>
+    <> 
+    <section className='heading'>
+      <h1>
+        <FaUser/>
+      </h1>
+      <p>Please create an account</p>
+    </section>
+
+    <section className='form'>
+    <div className='form-group'>
+    <form>
+      <input type="text" className='form-control' id="name" value={name} placeholder="enter your name" onChange={onChange}/>
+    </form>
+    </div>
+
+    <div className='form-group'>
+    <form>
+      <input type="email" className='form-control' id="email" value={email} placeholder="enter your email" onChange={onChange}/>
+    </form>
+    </div>
+
+    <div className='form-group'>
+    <form>
+      <input type="password" className='form-control' id="password" value={password} placeholder="enter your password" onChange={onChange}/>
+    </form>
+    </div>
+
+    <div className='form-group'>
+    <form>
+      <input type="password" className='form-control' id="password2" value={password2} placeholder="confirm your password" onChange={onChange}/>
+    </form>
+    </div>
+
+    </section>
+    
+    </>
   )
 }
