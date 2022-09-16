3397eae0f88da41864ed2aa2d3041206688f9c01
Angelo Maiele <angelomaiele@gmail.com>
2022-09-16 17:00:03 -0400

    more redux tool

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,56 @@
+// import e from 'express';
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+  const { email, password, } = formData;
+  const onChange = (e) => {
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+const onSubmit = (e) => {
+    e.preventDefault()
+}
+
   return (
-    <div>Login</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt />
+        </h1>
+        <p>Please Login</p>
+      </section>
+
+      <section className='form'>
+        <div className='form-group'>
+          <form onSubmit={onSubmit}>
+           
+            <div className='form-group'>
+              <input type="email" className='form-control' id="email" value={email} placeholder="enter your email" onChange={onChange} />
+            </div>
+            <div className='form-group'>
+              <input type="password" className='form-control' id="password" value={password} placeholder="enter your password" onChange={onChange} />
+            </div>
+    
+            <div className='form-group'>
+              <button type="submit" className='form-control btn btn-block'>submit</button>
+            </div>
+          </form>
+        </div>
+
+
+
+
+      </section>
+
+    </>
   )
 }
+
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,54 +1,61 @@
+// import e from 'express';
 import React from 'react'
 import { useEffect, useState } from 'react'
-import {FaUser} from 'react-icons/fa'
+import { FaUser } from 'react-icons/fa'
 
 export default function Register() {
-  const [formData, setFormData] =  useState({
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
     password2: "",
   })
-  const {name,email,password,password2} = formData;
-  const onChange = ()=>{
-
+  const { name, email, password, password2 } = formData;
+  const onChange = (e) => {
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
   }
+const onSubmit = (e) => {
+    e.preventDefault()
+}
+
   return (
-    <> 
-    <section className='heading'>
-      <h1>
-        <FaUser/>
-      </h1>
-      <p>Please create an account</p>
-    </section>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser />
+        </h1>
+        <p>Please create an account</p>
+      </section>
 
-    <section className='form'>
-    <div className='form-group'>
-    <form>
-      <input type="text" className='form-control' id="name" value={name} placeholder="enter your name" onChange={onChange}/>
-    </form>
-    </div>
+      <section className='form'>
+        <div className='form-group'>
+          <form onSubmit={onSubmit}>
+            <div className='form-group'>
+              <input type="text" className='form-control' id="name" value={name} placeholder="enter your name" onChange={onChange} />
+            </div>
+            <div className='form-group'>
+              <input type="email" className='form-control' id="email" value={email} placeholder="enter your email" onChange={onChange} />
+            </div>
+            <div className='form-group'>
+              <input type="password" className='form-control' id="password" value={password} placeholder="enter your password" onChange={onChange} />
+            </div>
+            <div className='form-group'>
+              <input type="password" className='form-control' id="password2" value={password2} placeholder="confirm your password" onChange={onChange} />
+            </div>
+            <div className='form-group'>
+              <button type="submit" className='form-control btn btn-block'>submit</button>
+            </div>
+          </form>
+        </div>
 
-    <div className='form-group'>
-    <form>
-      <input type="email" className='form-control' id="email" value={email} placeholder="enter your email" onChange={onChange}/>
-    </form>
-    </div>
 
-    <div className='form-group'>
-    <form>
-      <input type="password" className='form-control' id="password" value={password} placeholder="enter your password" onChange={onChange}/>
-    </form>
-    </div>
 
-    <div className='form-group'>
-    <form>
-      <input type="password" className='form-control' id="password2" value={password2} placeholder="confirm your password" onChange={onChange}/>
-    </form>
-    </div>
 
-    </section>
-    
+      </section>
+
     </>
   )
 }
